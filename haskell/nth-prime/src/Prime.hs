b0943b5f3b79bc652b73f5ac39e092ce5da40517
Tobias Klug <tbs.klg@gmail.com>
2023-07-06 12:58:19 +0200

    Solve nth prime

---
+++ haskell/nth-prime/src/Prime.hs
@@ -0,0 +1,8 @@
+module Prime (nth) where
+
+nth :: Int -> Maybe Integer
+nth 0 = Nothing
+nth n = Just $ primes !! (n - 1)
+ where
+    primes = sieve [2..]
+    sieve (x:xs) = x : sieve [y | y <- xs, y `mod` x /= 0]
