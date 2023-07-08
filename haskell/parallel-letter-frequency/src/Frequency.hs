65427cd6a9a220e32bd4872b830a2fd740853c94
Tobias Klug <tbs.klg@gmail.com>
2023-07-08 07:40:11 +0200

    Solve frequency

--- haskell/parallel-letter-frequency/src/Frequency.hs
+++ haskell/parallel-letter-frequency/src/Frequency.hs
@@ -1,7 +1,17 @@
 module Frequency (frequency) where
 
-import Data.Map  (Map)
-import Data.Text (Text)
+import Data.Char (isAlpha)
+import qualified Data.Map as M
+import qualified Data.Text as T
 
-frequency :: Int -> [Text] -> Map Char Int
-frequency nWorkers texts = error "You need to implement this function."
+frequency :: Int -> [T.Text] -> M.Map Char Int
+frequency _ = foldr ((M.unionWith (+) . countChars) . T.toLower) M.empty
+  where
+    countChars :: T.Text -> M.Map Char Int
+    countChars t = case T.uncons t of
+      Nothing -> M.empty
+      Just (h, r) -> addChar (h, r)
+
+    addChar (h, r)
+      | isAlpha h = M.insertWith (+) h 1 (countChars r)
+      | otherwise = countChars r
