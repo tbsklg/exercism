3272532571b996a44abefa3f14897aea3b2b7102
Tobias Klug <tbs.klg@gmail.com>
2023-08-08 08:00:11 +0200

    Update rational cipher

--- haskell/rotational-cipher/src/RotationalCipher.hs
+++ haskell/rotational-cipher/src/RotationalCipher.hs
@@ -1,10 +1,11 @@
 module RotationalCipher (rotate) where
-import Data.Char (ord, chr, isLower, isUpper)
+
+import Data.Char (chr, isLower, isUpper, ord)
 
 rotate :: Int -> String -> String
 rotate i = map rotateChar
-    where
-        rotateChar c
-            | isLower c = chr $ ord 'a' + (ord c - ord 'a' + i) `mod` 26
-            | isUpper c = chr $ ord 'A' + (ord c - ord 'A' + i) `mod` 26
-            | otherwise = c
+  where
+    rotateChar c
+      | isLower c = chr $ ord 'a' + (ord c - ord 'a' + i) `mod` 26
+      | isUpper c = chr $ ord 'A' + (ord c - ord 'A' + i) `mod` 26
+      | otherwise = c
