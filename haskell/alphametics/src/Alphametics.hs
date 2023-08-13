b002bcbc69bdab93a4e8f2c351c6b198ab08340f
Tobias Klug <tbs.klg@gmail.com>
2023-08-13 07:40:11 +0200

    WIP alphametics

--- haskell/alphametics/src/Alphametics.hs
+++ haskell/alphametics/src/Alphametics.hs
@@ -1,13 +1,13 @@
 module Alphametics (solve) where
 
-import Control.Monad (msum)
 import Data.Char (isSpace)
 import Data.Function (on)
 import Data.List (nub, nubBy)
-import Data.Maybe (fromMaybe)
-import Debug.Trace (traceShow)
+import Data.Maybe (fromMaybe, mapMaybe)
+import Safe (lastMay)
 import Text.Parsec (char, letter, many1, parse, string)
 import Text.Parsec.Combinator (sepBy)
+import Debug.Trace
 
 type Words = [String]
 
@@ -15,76 +15,76 @@ type Result = String
 
 type State = [(Char, Int)]
 
-type Carry = Int
-
 data Expression = Expression Words Result deriving (Show, Eq)
 
-solve :: String -> [Maybe [(Char, Int)]]
+solve :: String -> [Maybe State]
 solve puzzle = search ([], 0) (asExpression puzzle)
 
 search :: (State, Int) -> Expression -> [Maybe State]
-search (state, carry) expr@(Expression words result)
-  | isValid (state, carry) expr = traceShow (words, result) [Just state]
-  | otherwise = concatMap (`search` furtherExpression) $ possibilities
-  where
-    furtherExpression = Expression (filter (not . null) . map init $ words) (if null result then [] else init result)
-    subExpression = Expression (map (\x -> [last x]) words) [last result]
-    possibilities = candidates (state, carry) subExpression
+search (state, _) (Expression [] []) = [Just state]
+search (state, carry) expr =
+  concatMap
+    (\s -> search s (remaining expr))
+    . filter (\s -> isValidSubExpression s (chunk expr))
+    . candidates (state, carry)
+    $ chunk expr
 
-mergeState :: (State, Int) -> (State, Int) -> (State, Int)
-mergeState a ([], _) = a
-mergeState (a, c) (b : bs, c') = case lookup (fst b) a of
-  Nothing -> mergeState (b : a, c') (bs, c')
-  Just _ -> mergeState (a, c') (bs, c')
-
-isValid :: (State, Int) -> Expression -> Bool
-isValid ([], _) _ = False
-isValid (state, carry) (Expression [] []) = True
-isValid (state, carry) (Expression words result) = left == right
+remaining :: Expression -> Expression
+remaining (Expression [] []) = Expression [] []
+remaining (Expression [] r) = Expression [] (init r)
+remaining (Expression w r) = Expression left (init r)
   where
-    left = (+) carry . sum . map (asInt . map (`lookup` state)) $ words
-    right = asInt $ map (`lookup` state) result
+    left = filter (not . null) . map init $ w
+
+chunk :: Expression -> Expression
+chunk (Expression [] []) = Expression [] []
+chunk (Expression [] r) = Expression [] [last r]
+chunk (Expression w r) = Expression chunkedWords chunkedResult
+  where
+    chunkedWords = map (: []) . mapMaybe lastMay $ w
+    chunkedResult = case lastMay r of
+      Nothing -> []
+      Just x -> [x]
+
+isValidSubExpression :: (State, Int) -> Expression -> Bool
+isValidSubExpression ([], _) _ = False
+isValidSubExpression (state, _) (Expression w r) = traceShow (state, w, r, left `mod` 10 == right) left `mod` 10 == right
+  where
+    left = sum . map (asInt . map (`lookup` state)) $ w
+    right = asInt $ map (`lookup` state) r
 
 asInt :: [Maybe Int] -> Int
 asInt = foldl (\acc x -> acc * 10 + fromMaybe 0 x) 0
 
 candidates :: (State, Int) -> Expression -> [(State, Int)]
-candidates (state, carry) expr@(Expression words result) =  map candidate . filter isRelevant . allCombinations missingDigits $ letters
+candidates (state, carry) (Expression w r) = map candidate . allCombinations missingDigits $ letters
   where
     missingDigits = filter (`notElem` map snd state) [0 .. 9]
     keys = map fst state
-    letters = filter (`notElem` keys) . nub . concat $ words ++ [result]
-    isRelevant s
-      | null s = False
-      | otherwise = sum left `mod` 10 == right
-      where
-        left = map (asInt . map (`lookup` (s ++ state))) words
-        right = asInt $ map (`lookup` (s ++ state)) result
+    letters = filter (`notElem` keys) . nub . concat $ w ++ [r]
 
     candidate s = (s ++ state, newCarry)
       where
-        left = map (asInt . map (`lookup` (s ++ state))) words
-        right = asInt $ map (`lookup` (s ++ state)) result
-        totalSum = sum left + carry
-        (newCarry, value) = totalSum `divMod` 10
+        left = map (asInt . map (`lookup` (s ++ state))) w
+        newCarry = (sum left + carry) `div` 10
 
 asExpression :: String -> Expression
 asExpression xs = case parse expressionParser "" (filter (not . isSpace) xs) of
   Left err -> error (show err)
-  Right (words, result) -> Expression words result
+  Right (w, r) -> Expression w r
   where
     expressionParser = do
-      words <- sepBy wordParser (char '+')
+      w <- sepBy wordParser (char '+')
       _ <- string "=="
       result <- many1 letter
-      return (words, result)
+      return (w, result)
 
     wordParser = many1 letter
 
 allCombinations :: [Int] -> [Char] -> [[(Char, Int)]]
 allCombinations [] _ = [[]]
 allCombinations _ [] = [[]]
-allCombinations ds (x : xs) =
+allCombinations _ (x : xs) =
   filter
     (\ls -> length ls == (length . nubBy ((==) `on` snd) $ ls))
-    [(x, y) : zs | y <- ds, zs <- allCombinations ds xs]
+    [(x, y) : zs | y <- [0 .. 9], zs <- allCombinations [0 .. 9] xs]
