0c7f03e0728036c0ae411cdbcdcb8e11f1e13731
Tobias Klug <tbs.klg@gmail.com>
2023-08-18 07:40:11 +0200

    Solve resistor

---
+++ typescript/resistor-color/resistor-color.ts
@@ -0,0 +1,14 @@
+export const colorCode = (code: string): number => COLORS.findIndex(c => c === code)
+
+export const COLORS = [
+  'black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white',
+]
