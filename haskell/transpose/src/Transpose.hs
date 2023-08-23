565332360b6e41e3c71907547b014a0e2257afdd
Tobias Klug <tbs.klg@gmail.com>
2023-08-23 09:03:46 +0200

    Solve transpose

--- haskell/transpose/src/Transpose.hs
+++ haskell/transpose/src/Transpose.hs
@@ -5,9 +5,26 @@ import Data.Maybe (fromMaybe)
 
 transpose :: [String] -> [String]
 transpose [] = []
-transpose lines = go lines 0
+transpose lines = go . pad $ lines
   where
-    go (l:ls) g =
-      | 
+    go :: [String] -> [String]
+    go ls
+      | all null ls = []
+      | otherwise = concatMap line ls : go (map remaining ls)
 
-    currentLenght = length l
+    line :: String -> String
+    line l = case headMay l of
+      Nothing -> []
+      Just x -> [x]
+
+    remaining :: String -> String
+    remaining l = fromMaybe [] (tailMay l)
+
+    pad :: [String] -> [String]
+    pad [] = []
+    pad [x] = [x]
+    pad (x:y:xs)
+      | length x > length y = x : pad (y:xs)
+      | otherwise = (x ++ replicate (nextMaximum - length x) ' ') : pad (y:xs)
+      where
+        nextMaximum = maximum . map length $ (y:xs)
