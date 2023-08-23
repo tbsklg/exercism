4ab1c79345e7febfff43d86671f74eaf94c165a3
Tobias Klug <tbs.klg@gmail.com>
2023-08-23 07:44:45 +0200

    Update space age

--- typescript/space-age/space-age.ts
+++ typescript/space-age/space-age.ts
@@ -1,17 +1,17 @@
 export function age(planet: string, seconds: number): number {
+  const toEarthYearsFor = toEarthYears(seconds) 
+
   switch (planet) {
-    case 'mercury': return toEarthYears(0.2408467, seconds)
-    case 'venus': return toEarthYears(0.61519726, seconds) 
-    case 'earth': return toEarthYears (1.0, seconds) 
-    case 'mars': return toEarthYears (1.8808158, seconds) 
-    case 'jupiter': return toEarthYears (11.862615, seconds)
-    case 'saturn': return toEarthYears (29.447498, seconds)
-    case 'uranus': return toEarthYears (84.016846, seconds)
-    case 'neptune': return toEarthYears (164.79132, seconds)
+    case 'mercury': return toEarthYearsFor(0.2408467)
+    case 'venus': return toEarthYearsFor(0.61519726)
+    case 'earth': return toEarthYearsFor (1.0) 
+    case 'mars': return toEarthYearsFor (1.8808158) 
+    case 'jupiter': return toEarthYearsFor (11.862615)
+    case 'saturn': return toEarthYearsFor (29.447498)
+    case 'uranus': return toEarthYearsFor (84.016846)
+    case 'neptune': return toEarthYearsFor (164.79132)
     default: throw new Error('Invalid planet')
   }
 }
 
-const toEarthYears = (factor: number, seconds: number): number => {
-  return +(seconds / 3600 / 24 / (365.25 * factor)).toFixed(2)
-}
+const toEarthYears = (seconds: number) => (factor: number) => +(seconds / 3600 / 24 / (365.25 * factor)).toFixed(2)
