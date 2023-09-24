26e683e43fe024fb0127a4c7d7dc8267c22065a8
Tobias Klug <tbs.klg@gmail.com>
2023-09-24 21:41:11 +0200

    Update isogram

--- typescript/isogram/isogram.ts
+++ typescript/isogram/isogram.ts
@@ -1,4 +1,5 @@
 export const isIsogram = (word: string): boolean => {
+    // [^a-z] means any character that is not a-z
     const letters = word.toLowerCase().replace(/[^a-z]/g, '').split('');
     const uniqueLetters = new Set(letters);
     return letters.length === uniqueLetters.size;
