8f087e356089ee167c1ed731291a5f8e7efa5203
Tobias Klug <tbs.klg@gmail.com>
2023-10-14 08:32:45 +0200

    Update matching brackets

--- haskell/matching-brackets/src/Brackets.hs
+++ haskell/matching-brackets/src/Brackets.hs
@@ -1,26 +1,9 @@
 module Brackets (arePaired) where
 
 arePaired :: String -> Bool
-arePaired raw = validate (synth raw) []
+arePaired = null . foldr validate [] . filter (`elem` "[]{}()")
  where
-  validate :: String -> String -> Bool
-  validate [] [] = True
-  validate (x : xs) []
-    | isOpen x = validate xs [closing x]
-    | otherwise = False
-  validate (x : xs) s@(y : ys)
-    | isOpen x = validate xs (closing x : s)
-    | otherwise = x == y && validate xs ys
-  validate _ _ = False
-
-  isOpen :: Char -> Bool
-  isOpen x = x `elem` "({["
-
-  closing :: Char -> Char
-  closing '[' = ']'
-  closing '{' = '}'
-  closing '(' = ')'
-  closing x = x
-
-  synth :: String -> String
-  synth = filter (`elem` "[](){}")
+  validate '(' (')' : xs) = xs
+  validate '[' (']' : xs) = xs
+  validate '{' ('}' : xs) = xs
+  validate x xs = x : xs
