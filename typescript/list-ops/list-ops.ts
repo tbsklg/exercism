8d100bb097e3660ab4f9574adfa6489b6f047a62
Tobias Klug <tbs.klg@gmail.com>
2023-10-29 07:45:11 +0100

    Solve list ops

---
+++ typescript/list-ops/list-ops.ts
@@ -0,0 +1,53 @@
+export class List<T> {
+
+  private constructor(public values: T[]) { }
+
+  public static create<T>(...values: T[]): List<T> {
+    return new List(values)
+  }
+
+  forEach(cb: (item: any) => void): void {
+    for (const item of this.values) {
+      cb(item)
+    }
+  }
+
+  append(list: List<T>): List<T> {
+    return List.create(...this.values, ...list.values)
+  }
+
+  concat(list: List<List<T>>): List<T> {
+    return list.foldl((acc: List<T>, curr: List<T>) => acc.append(curr), List.create(...this.values))
+  }
+
+  filter<T>(fn: ((item: T) => Boolean)): T[] {
+    return this.foldr((acc: T[], curr: T) => fn(curr) ? [curr, ...acc] : acc, [])
+  }
+
+  length(): number {
+    return this.foldr((acc: number, _: any) => acc + 1, 0)
+  }
+
+  map<T>(fn: ((item: any) => T)): T[] {
+    return this.foldr((acc: T[], curr: any) => [fn(curr), ...acc], [])
+  }
+
+  foldl<T, R>(fn: ((acc: R, curr: T) => R), acc: R): R {
+    this.forEach((item: T) => {
+      acc = fn(acc, item)
+    })
+
+    return acc
+  }
+
+  foldr<T, R>(fn: ((acc: R, curr: T) => R), acc: R): R {
+    this.reverse()
+    return this.foldl(fn, acc)
+  }
+
+  reverse(): T[] {
+    this.values = this.values.reverse()
+    return this.values
+  }
+}
+
