52a928d25113fe9e01942a334b3d9376ca5de172
Tobias Klug <tbs.klg@gmail.com>
2023-11-04 07:45:11 +0100

    Solve raindrops (followup)

--- typescript/raindrops/raindrops.ts
+++ typescript/raindrops/raindrops.ts
@@ -1,7 +1,6 @@
 export const convert = (x: number): string => {
   const drops: [number, string][] = [[3, "Pling"], [5, "Plang"], [7, "Plong"]]
-  const rain: string =
-    drops.reduce((acc, [n, s]) => x % n === 0 ? acc + s : acc, "")
-
-  return rain === "" ? x.toString() : rain
+  return drops
+    .reduce((acc: string, [n, s]) => x % n === 0 ? acc + s : acc, "")
+    || x.toString()
 }
