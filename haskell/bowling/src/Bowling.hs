0e95c0232ab4e19df4cb0342fbd014244eccd42c
Tobias Klug <tbs.klg@gmail.com>
2023-11-05 07:45:11 +0100

    Solve bowling

--- haskell/bowling/src/Bowling.hs
+++ haskell/bowling/src/Bowling.hs
@@ -1,9 +1,48 @@
-module Bowling (score, BowlingError(..)) where
+module Bowling (score, BowlingError (..)) where
 
-data BowlingError = IncompleteGame
-                  | InvalidRoll { rollIndex :: Int, rollValue :: Int }
+data BowlingError
+  = IncompleteGame
+  | InvalidRoll {rollIndex :: Int, rollValue :: Int}
   deriving (Eq, Show)
 
-score :: [Int] -> Either BowlingError Int
-score rolls = error "You need to implement this function."
+type Roll = Int
+data State = State {frame :: Int, roll :: Roll, points :: Int}
+  deriving (Eq, Show)
 
+initialState :: State
+initialState = State{frame = 1, roll = 0, points = 0}
+
+score :: [Roll] -> Either BowlingError Int
+score rolls = case validate rolls of
+  Left e -> Left e
+  Right rs -> play rs initialState
+ where
+  play [] State{frame = 11, points = p} = Right p
+  play [s1, s2] State{frame = 10, points = p}
+    | s1 == 10 || s1 + s2 == 10 = Left IncompleteGame
+    | otherwise = Right $ p + s1 + s2
+  play [_, _, s3, _] State{frame = 10, roll = r} =
+    Left $ InvalidRoll{rollIndex = r + 3, rollValue = s3}
+  play [10, s1, s2] State{frame = 10, roll = r, points = p}
+    | s1 /= 10, s1 + s2 > 10 = Left $ InvalidRoll{rollIndex = r + 2, rollValue = s2}
+    | otherwise = Right $ p + 10 + s1 + s2
+  play [s1, s2, s3] State{frame = 10, roll = r, points = p}
+    | s1 + s2 == 10 = Right $ p + 10 + s3
+    | otherwise = Left $ InvalidRoll{rollIndex = r + 2, rollValue = s2}
+  play (s1 : s2 : s3 : rs) state@State{frame = f, roll = r, points = p}
+    | s1 == 10 = play (s2 : s3 : rs) state{frame = f + 1, roll = r + 1, points = p + 10 + s2 + s3}
+    | s1 + s2 == 10 = play (s3 : rs) state{frame = f + 1, roll = r + 2, points = p + 10 + s3}
+    | s1 + s2 < 10 = play (s3 : rs) state{frame = f + 1, roll = r + 2, points = p + s1 + s2}
+    | otherwise = Left $ InvalidRoll{rollIndex = r, rollValue = s2}
+  play (s1 : s2 : rs) state@State{frame = f, roll = r, points = p}
+    | s1 == 10 = play (s2 : rs) state{frame = f + 1, roll = r + 1, points = p + 10 + s2}
+    | s1 + s2 < 10 = play rs state{frame = f + 1, roll = r + 2, points = p + s1 + s2}
+    | otherwise = Left $ InvalidRoll{rollIndex = r + 1, rollValue = s2}
+  play _ _ = Left IncompleteGame
+
+validate :: [Roll] -> Either BowlingError [Roll]
+validate = mapM validate' . zip [0 ..]
+ where
+  validate' (i, r)
+    | r < 0 || r > 10 = Left $ InvalidRoll{rollIndex = i, rollValue = r}
+    | otherwise = Right r
