e98a2946714e2c00289dadf19bb5eb36ca30bc51
Tobias Klug <tbs.klg@gmail.com>
2023-11-13 06:45:11 +0100

    Solve scrabble

---
+++ typescript/scrabble-score/scrabble-score.ts
@@ -0,0 +1,33 @@
+export const score = (score: (string | undefined)): number =>
+  [...(score ?? '')].reduce((acc, curr) => acc + (scrabbleScore.get(curr.toUpperCase()) ?? 0), 0)
+
+const scrabbleScore: Map<string, number> = new Map(
+  [
+    ['A', 1],
+    ['E', 1],
+    ['I', 1],
+    ['O', 1],
+    ['U', 1],
+    ['L', 1],
+    ['N', 1],
+    ['R', 1],
+    ['S', 1],
+    ['T', 1],
+    ['D', 2],
+    ['G', 2],
+    ['B', 3],
+    ['C', 3],
+    ['M', 3],
+    ['P', 3],
+    ['F', 4],
+    ['H', 4],
+    ['V', 4],
+    ['W', 4],
+    ['Y', 4],
+    ['K', 5],
+    ['J', 8],
+    ['X', 8],
+    ['Q', 10],
+    ['Z', 10]
+  ]
+) 
