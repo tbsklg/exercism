da11908435ec5403ac9ded0a7f6aca485d231a3f
Tobias Klug <tbs.klg@gmail.com>
2023-11-20 06:45:11 +0100

    Refactor sublist

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -7,35 +7,20 @@ pub enum Comparison {
 }
 
 pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    if _first_list == _second_list {
-        return Comparison::Equal;
-    }
-
-    if _first_list.is_empty() {
-        return Comparison::Sublist;
-    }
-
-    if _second_list.is_empty() {
-        return Comparison::Superlist;
-    }
-
-    if _first_list.len() > _second_list.len() {
-        if _first_list
-            .windows(_second_list.len())
-            .any(|w| w == _second_list)
-        {
-            return Comparison::Superlist;
-        } else {
-            return Comparison::Unequal;
-        }
-    } else {
-        if _second_list
+    let sublist = _first_list.is_empty()
+        || _second_list
             .windows(_first_list.len())
-            .any(|w| w == _first_list)
-        {
-            return Comparison::Sublist;
-        } else {
-            return Comparison::Unequal;
-        }
+            .any(|w| w == _first_list);
+
+    let superlist = _second_list.is_empty()
+        || _first_list
+            .windows(_second_list.len())
+            .any(|w| w == _second_list);
+
+    match (sublist, superlist) {
+        (true, true) => Comparison::Equal,
+        (true, false) => Comparison::Sublist,
+        (false, true) => Comparison::Superlist,
+        (false, false) => Comparison::Unequal,
     }
 }
