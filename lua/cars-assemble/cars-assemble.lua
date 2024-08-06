16a8db6407ce202e0bf28b77a1d8fc3563232dba
Tobias Klug <tbs.klg@gmail.com>
2024-08-06 22:17:46 +0200

    Solve cars-assemble

---
+++ lua/cars-assemble/cars-assemble.lua
@@ -0,0 +1,18 @@
+local cars = {}
+
+-- returns the amount of working cars produced by the assembly line every hour
+function cars.calculate_working_cars_per_hour(production_rate, success_rate)
+  return production_rate * success_rate / 100
+end
+
+-- returns the amount of working cars produced by the assembly line every minute
+function cars.calculate_working_cars_per_minute(production_rate, success_rate)
+  return cars.calculate_working_cars_per_hour(production_rate, success_rate) // 60
+end
+
+-- returns the cost of producing the given number of cars
+function cars.calculate_cost(cars_count)
+  return (cars_count // 10) * 95000 + (cars_count % 10) * 10000
+end
+
+return cars
