e8463a055f5bc1043ac9ec000ba6f686d7412aa3
Tobias Klug <tbs.klg@gmail.com>
2024-09-24 05:30:00 +0200

    Solve Word Search

--- haskell/word-search/src/WordSearch.hs
+++ haskell/word-search/src/WordSearch.hs
@@ -1,52 +1,54 @@
 module WordSearch (search, CharPos (..), WordPos (..)) where
 
-import Control.Applicative
-import Data.List (findIndex, isPrefixOf, tails, transpose)
+import Control.Applicative ()
+import Data.List (elemIndices)
 
 data CharPos = CharPos {col :: Int, row :: Int} deriving (Eq, Show)
+
 data WordPos = WordPos {start :: CharPos, end :: CharPos} deriving (Eq, Show)
 
 search :: [String] -> [String] -> [(String, Maybe WordPos)]
-search [] _ = []
 search _ [] = []
 search grid (w : ws) =
-  let
-    horizontals = indexed grid
-    verticals = indexed $ transpose grid
+  let startPositions = case safeHead w of
+        Just c -> findPositions c grid
+        Nothing -> []
 
-    scanHor [] = Nothing
-    scanHor ((i, l) : ls) =
-      case findWord w l of
-        Nothing -> scanHor ls
-        Just (f, t) -> Just (w, Just $ WordPos (CharPos f i) (CharPos t i))
+      inBounds (r, c) = r >= 0 && r < length grid && c >= 0 && c < length (head grid)
 
-    scanVer [] = Nothing
-    scanVer ((i, l) : ls) =
-      case findWord w l of
-        Nothing -> scanVer ls
-        Just (f, t) -> Just (w, Just $ WordPos (CharPos i f) (CharPos i t))
-   in
-    case scanHor horizontals <|> scanVer verticals of
-      Nothing -> (w, Nothing) : search grid ws
-      Just r -> r : search grid ws
+      go [] = []
+      go ((r, c) : ps) =
+        let leftToRight = [(r, c') | c' <- [c .. c + length w - 1], inBounds (r, c')]
+            rightToLeft = [(r, c') | c' <- [c, c - 1 .. c - length w - 1], inBounds (r, c')]
+            topToBottom = [(r', c) | r' <- [r .. r + length w - 1], inBounds (r', c)]
+            bottomToTop = [(r', c) | r' <- [r, r - 1 .. r - length w + 1], inBounds (r', c)]
+            topLeftBottomRight = filter inBounds $ zip [r .. r + length w - 1] [c .. c + length w - 1]
+            bottomRightTopLeft = filter inBounds $ zip [r, r - 1 .. r - length w + 1] [c, c - 1 .. c - length w - 1]
+            topRightBottomLeft = filter inBounds $ zip [r .. r + length w - 1] [c, c - 1 .. c - length w - 1]
+            bottomLeftTopRight = filter inBounds $ zip [r, r - 1 .. r - length w + 1] [c .. c + length w - 1]
 
-indexed :: [a] -> [(Int, a)]
-indexed = zip [1 ..]
+            foundWords =
+              filter
+                (\ys -> map (\(x, y) -> grid !! x !! y) ys == w)
+                [leftToRight, rightToLeft, topToBottom, bottomToTop, topLeftBottomRight, bottomRightTopLeft, topRightBottomLeft, bottomLeftTopRight]
+         in case foundWords of
+              [] -> go ps
+              (ys : _) -> (w, Just $ WordPos {start = s, end = e}) : go ps
+                where
+                  s = CharPos {col = c + 1, row = r + 1}
+                  e = CharPos {col = snd (last ys) + 1, row = fst (last ys) + 1}
 
-findWord :: String -> String -> Maybe (Int, Int)
-findWord w l = rightToLeft w l <|> leftToRight w l
+      result = go startPositions
+   in if null startPositions || null result
+        then (w, Nothing) : search grid ws
+        else go startPositions ++ search grid ws
 
-rightToLeft :: String -> String -> Maybe (Int, Int)
-rightToLeft = findWordInLine
+findPositions :: Char -> [String] -> [(Int, Int)]
+findPositions c xs =
+  let go [] = []
+      go ((i, l) : ls) = [(i, j) | j <- elemIndices c l] ++ go ls
+   in go (zip [0 ..] xs)
 
-leftToRight :: String -> String -> Maybe (Int, Int)
-leftToRight w l = case findWordInLine (reverse w) l of
-  Nothing -> Nothing
-  Just (f, t) -> Just (t, f)
-
-findWordInLine :: String -> String -> Maybe (Int, Int)
-findWordInLine w l
-  | length w > length l = Nothing
-  | otherwise = case findIndex (isPrefixOf w) (tails l) of
-      Nothing -> Nothing
-      Just i -> Just (i + 1, i + length w)
+safeHead :: [a] -> Maybe a
+safeHead [] = Nothing
+safeHead (x : _) = Just x
