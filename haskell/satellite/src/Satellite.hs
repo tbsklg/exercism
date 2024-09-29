8d483064549daf57aba97b8367b9cd606924672d
Tobias Klug <tbs.klg@gmail.com>
2024-09-29 05:30:00 +0200

    Solve Satellite

---
+++ haskell/satellite/src/Satellite.hs
@@ -0,0 +1,29 @@
+module Satellite (treeFromTraversals) where
+
+import BinaryTree (BinaryTree (..))
+import Data.List (sort)
+
+treeFromTraversals :: (Show a, Ord a) => [a] -> [a] -> Maybe (BinaryTree a)
+treeFromTraversals [] [] = Nothing
+treeFromTraversals preorder inorder
+  | sort preorder /= sort inorder = Nothing
+  | hasDuplicates preorder || hasDuplicates inorder = Nothing
+  | otherwise = go preorder inorder
+  where
+    go :: (Show a, Ord a) => [a] -> [a] -> Maybe (BinaryTree a)
+    go [] [] = Just Leaf
+    go po io =
+      let x = head po
+          leftTree =
+            let po' = filter (`elem` io') po
+                io' = takeWhile (/= x) io
+             in go po' io'
+          rightTree =
+            let po' = filter (`elem` io') po
+                io' = tail $ dropWhile (/= x) io
+             in go po' io'
+       in Branch <$> leftTree <*> pure x <*> rightTree
+
+hasDuplicates :: (Eq a) => [a] -> Bool
+hasDuplicates [] = False
+hasDuplicates (x : xs) = x `elem` xs || hasDuplicates xs
