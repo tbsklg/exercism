f7241bf3be945c9482a72c64c10ea64e8c49be29
Tobias Klug <tbs.klg@gmail.com>
2024-10-06 05:30:00 +0200

    Solve Spiral

--- rust/spiral-matrix/src/lib.rs
+++ rust/spiral-matrix/src/lib.rs
@@ -10,31 +10,43 @@ pub fn spiral_matrix(size: u32) -> Vec<Vec<u32>> {
     let mut matrix = vec![vec![0; size as usize]; size as usize];
 
     let mut r = 0usize;
+    let mut r1 = size as usize;
+
     let mut c = 0usize;
-    let mut n = 1u32;
+    let mut c1 = size as usize;
 
-    while n <= size * size {
-        while c < (size - 1) as usize && matrix[r][c] == 0 {
-            matrix[r][c] = n;
-            n += 1;
-            c += 1;
+    let mut v = 1;
+
+    while v <= size * size {
+        // right
+        for n in c..c1 {
+            matrix[r][n] = v;
+            v += 1;
         }
 
-        while r < (size - 1) as usize && matrix[r][c] == 0 {
-            matrix[r][c] = n;
-            n += 1;
-            r += 1;
+        // down
+        for n in r + 1..r1 {
+            matrix[n][c1 - 1] = v;
+            v += 1;
         }
-        while c < (size - 1) as usize && matrix[r][c] == 0 {
-            matrix[r][c] = n;
-            n += 1;
-            c -= 1;
+
+        // left
+        for n in (c..c1 - 1).rev() {
+            matrix[r1 - 1][n] = v;
+            v += 1;
         }
-        while r < (size - 1) as usize && matrix[r][c] == 0 {
-            matrix[r][c] = n;
-            n += 1;
-            r -= 1;
+
+        // up
+        for n in (r + 1..r1 - 1).rev() {
+            matrix[n][c] = v;
+            v += 1;
         }
+
+        // update bounds
+        r += 1;
+        r1 -= 1;
+        c += 1;
+        c1 -= 1;
     }
 
     matrix
