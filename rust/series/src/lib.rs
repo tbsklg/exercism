82f0cd5f51b2e848bd358773cce92f4d422ffd55
Tobias Klug <tbs.klg@gmail.com>
2024-10-22 05:30:00 +0200

    Solve series

--- rust/series/src/lib.rs
+++ rust/series/src/lib.rs
@@ -1,13 +1,8 @@
 pub fn series(digits: &str, len: usize) -> Vec<String> {
-    let mut r = vec![];
-
-    if len > digits.len() {
-        return r;
-    }
-
-    for i in 0..=digits.len() - len {
-        r.push(digits[i..i + len].to_string());
-    }
-
-    return r;
+    digits
+        .chars()
+        .collect::<Vec<char>>()
+        .windows(len)
+        .map(|w| w.iter().collect())
+        .collect()
 }
