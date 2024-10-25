232c5cfbd82a66cd8b71b81b10a16e1a45399eba
Tobias Klug <tbs.klg@gmail.com>
2024-10-25 05:30:00 +0200

    Start arithmetic puzzle

--- haskell/alphametics/src/Alphametics.hs
+++ haskell/alphametics/src/Alphametics.hs
@@ -8,13 +8,11 @@ type Result = String
 data Ast = Literal String | Binary Ast Operator Ast deriving (Show, Eq)
 data Operator = Plus deriving (Show, Eq)
 
--- >>> splitOn "==" "a + b + c == d"
--- ["a + b + c "," d"]
 solve :: String -> Maybe [(Char, Int)]
 solve puzzle = error "You need to implement this function."
 
 -- parse a string to and ast and a result
--- "a + b + c" = (+ (+ (a b)) c)
-parse :: String -> (Ast, String)
+-- "a + b + c" =
+parse :: String -> (Ast, Result)
 parse xs = undefined
 
