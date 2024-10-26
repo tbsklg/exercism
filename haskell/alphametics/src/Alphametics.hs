48d224b02fb4fc53ed44ce6fadff3debe685be21
Tobias Klug <tbs.klg@gmail.com>
2024-10-26 05:30:00 +0200

    Start arithmetic puzzle

--- haskell/alphametics/src/Alphametics.hs
+++ haskell/alphametics/src/Alphametics.hs
@@ -1,9 +1,6 @@
 module Alphametics (solve) where
 
-import Data.List.Split (splitOn)
-
-type State = [(String, Int)]
-type Result = String
+import Data.List.Split
 
 data Ast = Literal String | Binary Ast Operator Ast deriving (Show, Eq)
 data Operator = Plus deriving (Show, Eq)
@@ -11,8 +8,12 @@ data Operator = Plus deriving (Show, Eq)
 solve :: String -> Maybe [(Char, Int)]
 solve puzzle = error "You need to implement this function."
 
--- parse a string to and ast and a result
--- "a + b + c" =
-parse :: String -> (Ast, Result)
-parse xs = undefined
+parseAst :: String -> Maybe Ast
+parseAst xs = 
+  let
+    [l, r] = splitOn " == " xs
+    operands = splitOn " + " l
+  in
+    Just $ foldr ((\acc c -> Binary acc Plus c) . Literal) (Literal r) operands
+
 
