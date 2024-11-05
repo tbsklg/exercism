1e159338dd2193ac6dd81af59fbba8c26bb8ee28
Tobias Klug <tbs.klg@gmail.com>
2024-11-05 06:30:00 +0100

    Solve alphametics

--- haskell/alphametics/src/Alphametics.hs
+++ haskell/alphametics/src/Alphametics.hs
@@ -1,12 +1,13 @@
 module Alphametics (solve) where
 
-import Data.List
+import Data.List ( find, delete, nub )
 import Data.List.Split (splitOn)
-import Data.Char
-import Data.Maybe
+import Data.Maybe ( fromJust )
 
 type Equation = (Operands, Result)
+
 type Operands = [String]
+
 type Result = String
 
 type Combinations = [[Int]]
@@ -14,7 +15,10 @@ type Combinations = [[Int]]
 solve :: String -> Maybe [(Char, Int)]
 solve puzzle = case parse puzzle of
   Left _ -> Nothing
-  Right equation -> find (`testCombination` equation) $ possibleCombinations equation
+  Right equation ->
+    find (`testCombination` equation) $
+    filter (not . hasLeadingZero equation) $
+    possibleCombinations equation
 
 parse :: String -> Either String (Operands, Result)
 parse xs = case splitOn " == " xs of
@@ -24,23 +28,25 @@ parse xs = case splitOn " == " xs of
   [operands, result] -> Right (splitOn " + " operands, result)
   _ -> Left "Invalid input, expected 'operands == result'"
 
+hasLeadingZero :: Equation -> [(Char, Int)] -> Bool
+hasLeadingZero (operands, result) state =
+  let flhs = map head operands
+      frhs = head result
+   in any ((== 0) . fromJust . flip lookup state) (flhs ++ [frhs])
+
 possibleCombinations :: Equation -> [[(Char, Int)]]
-possibleCombinations (os, r) = 
-  let
-    a = nub (r ++ concat os)
-    c = combinations $ length a
-  in
-    map (zip a) c
+possibleCombinations (os, r) =
+  let a = nub (r ++ concat os)
+      c = combinations $ length a
+   in map (zip a) c
 
 testCombination :: [(Char, Int)] -> Equation -> Bool
 testCombination state (operands, result) =
-  let
-    lhs :: Int
-    lhs = sum $ map (asInt . map (fromJust . flip lookup state)) operands
-    rhs :: Int
-    rhs = asInt $ map (fromJust . flip lookup state) result
-  in
-    lhs == rhs
+  let lhs :: Int
+      lhs = sum $ map (asInt . map (fromJust . flip lookup state)) operands
+      rhs :: Int
+      rhs = asInt $ map (fromJust . flip lookup state) result
+   in lhs == rhs
 
 asInt :: [Int] -> Int
 asInt = read . concatMap show
@@ -52,4 +58,3 @@ combinations l = go l [0 .. 9] []
     go l xs c
       | l == 0 = [c]
       | otherwise = concatMap (\x -> go (l - 1) (delete x xs) (x : c)) xs
-
