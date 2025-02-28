c55d1faa34301f01db704bd36e9fe68d32e4237d
Tobias Klug <tbs.klg@gmail.com>
2025-02-28 15:57:59 +0100

    Update accumulate

--- rust/accumulate/src/lib.rs
+++ rust/accumulate/src/lib.rs
@@ -1,6 +1,9 @@
-pub fn map<F, T, U>(input: Vec<T>, function: F) -> Vec<U>
+pub fn map<F, T, U>(input: Vec<T>, mut function: F) -> Vec<U>
 where
     F: FnMut(T) -> U,
 {
-    input.into_iter().map(function).collect()
+    input.into_iter().fold(vec![], |mut acc, curr| {
+        acc.push(function(curr));
+        acc
+    })
 }
