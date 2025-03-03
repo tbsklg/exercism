65db4d0f017ea36df712e11f5e0341d6e28917ef
Tobias Klug <tbs.klg@gmail.com>
2025-03-03 22:17:10 +0100

    WIP: Allergies

---
+++ rust/allergies/src/lib.rs
@@ -0,0 +1,41 @@
+pub struct Allergies {
+    score: u32
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Allergen {
+    Eggs,
+    Peanuts,
+    Shellfish,
+    Strawberries,
+    Tomatoes,
+    Chocolate,
+    Pollen,
+    Cats,
+}
+
+impl Allergies {
+    pub fn new(score: u32) -> Self {
+        Self {
+            score
+        }
+    }
+
+    pub fn is_allergic_to(&self, allergen: &Allergen) -> bool {
+        self.allergies().iter().position(|a| a == allergen)
+        false
+    }
+
+    pub fn allergies(&self) -> Vec<Allergen> {
+        vec![
+            Allergen::Eggs,
+            Allergen::Peanuts,
+            Allergen::Shellfish,
+            Allergen::Strawberries,
+            Allergen::Tomatoes,
+            Allergen::Chocolate,
+            Allergen::Pollen,
+            Allergen::Cats,
+        ]
+    }
+}
