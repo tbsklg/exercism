6e1e22fe3e40196c409eb8b0c60f27d2d9e93a56
Tobias Klug <tbs.klg@gmail.com>
2025-03-05 11:42:46 +0100

    Solve proverb

---
+++ rust/proverb/src/lib.rs
@@ -0,0 +1,26 @@
+use std::iter::zip;
+
+pub fn build_proverb(l: &[&str]) -> String {
+    if l.is_empty() {
+        return String::new();
+    }
+
+    let mut phrases = zip(l, &l[1..]).fold(vec![],
+        |mut acc, curr| {
+            let phrase = lost_for(curr.0, curr.1);
+            acc.push(phrase);
+            acc
+        },
+    );
+
+    phrases.push(end(l[0]));
+    phrases.join("\n")
+}
+
+fn lost_for(x: &str, y: &str) -> String {
+    format!("For want of a {x} the {y} was lost.")
+}
+
+fn end(x: &str) -> String {
+    format!("And all for the want of a {x}.")
+}
