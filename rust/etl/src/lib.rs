8aff50e2bc90b491a332c9971e4a70ca88268fc7
Tobias Klug <tbs.klg@gmail.com>
2025-03-13 07:40:10 +0100

    Update etl

--- rust/etl/src/lib.rs
+++ rust/etl/src/lib.rs
@@ -3,9 +3,7 @@ use std::collections::BTreeMap;
 pub fn transform(h: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
     h.iter().fold(BTreeMap::new(), |mut acc, (k, v)| {
         for p in v {
-            p.to_lowercase()
-                .next()
-                .and_then(|new_key| acc.insert(new_key, *k));
+            acc.insert(p.to_ascii_lowercase(), *k);
         }
 
         acc
