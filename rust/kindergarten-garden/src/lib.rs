56ff71ef91901b391b7f86be2ee3fe2e569db576
Tobias Klug <tbs.klg@gmail.com>
2025-03-31 06:30:00 +0200

    Update kindergarten_garden

--- rust/kindergarten-garden/src/lib.rs
+++ rust/kindergarten-garden/src/lib.rs
@@ -4,25 +4,22 @@ static CHILDREN: [&str; 12] = [
 ];
 
 pub fn plants(diagram: &str, student: &str) -> Vec<&'static str> {
-    let child_pos: usize = CHILDREN.iter().position(|&name| name == student).unwrap() * 2;
+    let student_idx = CHILDREN
+        .iter()
+        .position(|&name| name == student)
+        .map(|pos| pos * 2)
+        .unwrap_or_else(|| panic!("Student not found"));
 
     diagram
         .lines()
         .flat_map(|row| {
-            let chars: Vec<char> = row.chars().collect();
-            let first_plant = chars[child_pos];
-            let second_plant = chars[child_pos + 1];
-            vec![plant(first_plant), plant(second_plant)]
+            row.chars().skip(student_idx).take(2).map(|c| match c {
+                'G' => "grass",
+                'C' => "clover",
+                'R' => "radishes",
+                'V' => "violets",
+                _ => unreachable!(),
+            })
         })
         .collect()
-}
-
-fn plant(encoding: char) -> &'static str {
-    match encoding {
-        'G' => "grass",
-        'C' => "clover",
-        'R' => "radishes",
-        'V' => "violets",
-        _ => unreachable!(),
-    }
 }
