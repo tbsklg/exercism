ce0986482f349a4c045a21771da24ebecef216ca
Tobias Klug <tbs.klg@gmail.com>
2025-04-25 06:30:00 +0200

    Update luhn

--- typescript/luhn/luhn.ts
+++ typescript/luhn/luhn.ts
@@ -1,25 +1,26 @@
 export function valid(digitString: string): boolean {
-  const digits = (digitString.match(/\d/g) ?? []).map(Number)
+  const digits = (digitString.match(/\d/g) ?? []).map(Number);
 
   return (
     digits.length > 1 &&
     digits.length === [...digitString].filter(notIsWhitespace).length &&
-    digits
-      .reverse()
-      .map((x, i) => {
-        if (even(i)) {
-          return x;
-        }
+    divisibleByTen(
+      digits
+        .reverse()
+        .map((x, i) => {
+          if (even(i)) {
+            return x;
+          }
 
-        const doubled = x * 2;
-        return doubled > 9 ? doubled - 9 : doubled;
-      })
-      .reduce((acc, curr) => acc + curr, 0) %
-      10 ===
-      0
+          const doubled = x * 2;
+          return doubled > 9 ? doubled - 9 : doubled;
+        })
+        .reduce((acc, curr) => acc + curr, 0),
+    )
   );
 }
 
+const divisibleByTen = (x: number): boolean => x % 10 === 0;
 const notIsWhitespace = (x: string): boolean => !isWhitespace(x);
 const isWhitespace = (x: string): boolean => x === " ";
 const even = (x: number): boolean => x % 2 === 0;
