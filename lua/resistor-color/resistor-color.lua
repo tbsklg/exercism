52fc96e097329728a4b4a77e2d45010a08c1d0da
Tobias Klug <tbs.klg@gmail.com>
2025-05-06 06:30:00 +0200

    Solve resistor-color

---
+++ lua/resistor-color/resistor-color.lua
@@ -0,0 +1,14 @@
+return {
+  color_code = function(color)
+    if color == 'black' then return 0 end
+    if color == 'brown' then return 1 end
+    if color == 'red' then return 2 end
+    if color == 'orange' then return 3 end
+    if color == 'yellow' then return 4 end
+    if color == 'green' then return 5 end
+    if color == 'blue' then return 6 end
+    if color == 'violet' then return 7 end
+    if color == 'grey' then return 8 end
+    if color == 'white' then return 9 end
+  end
+}
