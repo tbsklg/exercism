2875d06b633b6c5a8277291446dba83c4984aa75
Tobias Klug <tbs.klg@gmail.com>
2025-05-10 06:30:00 +0200

    Update resistor colors

--- lua/resistor-color-duo/resistor-color-duo.lua
+++ lua/resistor-color-duo/resistor-color-duo.lua
@@ -1,30 +1,23 @@
 return {
 	value = function(colors)
+		local colorMap = {
+			black = "0",
+			brown = "1",
+			red = "2",
+			orange = "3",
+			yellow = "4",
+			green = "5",
+			blue = "6",
+			violet = "7",
+			grey = "8",
+			white = "9",
+		}
+
 		local band = ""
 		for i = 1, math.min(2, #colors) do
-			local color = colors[i]
-			if color == "black" then
-				band = band .. "0"
-			elseif color == "brown" then
-				band = band .. "1"
-			elseif color == "red" then
-				band = band .. "2"
-			elseif color == "orange" then
-				band = band .. "3"
-			elseif color == "yellow" then
-				band = band .. "4"
-			elseif color == "green" then
-				band = band .. "5"
-			elseif color == "blue" then
-				band = band .. "6"
-			elseif color == "violet" then
-				band = band .. "7"
-			elseif color == "grey" then
-				band = band .. "8"
-			elseif color == "white" then
-				band = band .. "9"
-			end
+			band = band .. (colorMap[colors[i]] or "")
 		end
+
 		return tonumber(band)
 	end,
 }
