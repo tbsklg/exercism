52cc118e67596d7cf7dbe37a7a068bc3ed29813c
tbsklg <tbs.klg@gmail.com>
2025-05-26 06:00:00 +0200

    Init rna

---
+++ rust/rna-transcription/src/lib.rs
@@ -0,0 +1,25 @@
+#[derive(Debug, PartialEq, Eq)]
+pub struct Dna;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Rna;
+
+impl Dna {
+    pub fn new(dna: &str) -> Result<Dna, usize> {
+        todo!(
+            "Construct new Dna from '{dna}' string. If string contains invalid nucleotides return index of first invalid nucleotide"
+        );
+    }
+
+    pub fn into_rna(self) -> Rna {
+        todo!("Transform Dna {self:?} into corresponding Rna");
+    }
+}
+
+impl Rna {
+    pub fn new(rna: &str) -> Result<Rna, usize> {
+        todo!(
+            "Construct new Rna from '{rna}' string. If string contains invalid nucleotides return index of first invalid nucleotide"
+        );
+    }
+}
