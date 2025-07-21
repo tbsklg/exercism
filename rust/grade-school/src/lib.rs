3516e8266c1b8ae36ee447ec62c0449bff95ebb7
Tobias Klug <tbs.klg@gmail.com>
2025-07-21 06:30:00 +0200

    Grade is empty

---
+++ rust/grade-school/src/lib.rs
@@ -0,0 +1,27 @@
+pub struct School {
+    grade: u8,
+}
+
+impl School {
+    pub fn new() -> School {
+        Self {
+            grade: 1,
+        }
+    }
+
+    pub fn add(&mut self, grade: u32, student: &str) {
+        todo!("Add {student} to the roster for {grade}")
+    }
+
+    pub fn grades(&self) -> Vec<u32> {
+        todo!()
+    }
+
+    // If `grade` returned a reference, `School` would be forced to keep a `Vec<String>`
+    // internally to lend out. By returning an owned vector of owned `String`s instead,
+    // the internal structure can be completely arbitrary. The tradeoff is that some data
+    // must be copied each time `grade` is called.
+    pub fn grade(&self, grade: u32) -> Vec<String> {
+        vec![]
+    }
+}
