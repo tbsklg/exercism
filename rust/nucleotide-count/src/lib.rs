4cee35023dec2ef3a98bca3e911f786cb7217015
Tobias Klug <tbs.klg@gmail.com>
2025-08-19 06:30:00 +0200

    Count invalid dna

--- rust/nucleotide-count/src/lib.rs
+++ rust/nucleotide-count/src/lib.rs
@@ -4,8 +4,14 @@ pub fn count(nucleotide: char, dna: &str) -> Result<usize, char> {
     if nucleotide == 'X' {
         return Err('X');
     }
+    
+    for c in dna.chars() {
+        if c != 'A' && c != 'C' && c != 'G' && c != 'T' {
+            return Err(c);
+        }
+    }
 
-    Ok(0)
+    Ok(dna.chars().filter(|&c| c == nucleotide).count())
 }
 
 pub fn nucleotide_counts(dna: &str) -> Result<HashMap<char, usize>, char> {
