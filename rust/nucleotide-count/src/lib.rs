e0e3d84be5c54a7a8ff4746a7daab567fe166ef2
Tobias Klug <tbs.klg@gmail.com>
2025-08-24 16:45:25 +0200

    Refactor nucleotide count

--- rust/nucleotide-count/src/lib.rs
+++ rust/nucleotide-count/src/lib.rs
@@ -1,37 +1,15 @@
 use std::collections::HashMap;
 
 pub fn count(nucleotide: char, dna: &str) -> Result<usize, char> {
-    if !matches!(nucleotide, 'A' | 'C' | 'G' | 'T') {
-        return Err(nucleotide);
-    }
-
-    let mut count = 0;
-    for c in dna.chars() {
-        match c {
-            'A' | 'C' | 'G' | 'T' => {
-                if c == nucleotide {
-                    count += 1;
-                }
-            }
-            invalid => return Err(invalid),
-        }
-    }
-
-    Ok(count)
+    let mut counts = nucleotide_counts(dna)?;
+    counts.remove(&nucleotide).ok_or(nucleotide)
 }
 
 pub fn nucleotide_counts(dna: &str) -> Result<HashMap<char, usize>, char> {
     let mut counts = HashMap::from([('A', 0), ('C', 0), ('G', 0), ('T', 0)]);
 
     for c in dna.chars() {
-        match c {
-            'A' | 'C' | 'G' | 'T' => {
-                *counts
-                    .get_mut(&c)
-                    .expect("Valid nucleotide should exist in map") += 1;
-            }
-            invalid => return Err(invalid),
-        }
+        counts.get_mut(&c).map(|count| *count += 1).ok_or(c)?
     }
 
     Ok(counts)
