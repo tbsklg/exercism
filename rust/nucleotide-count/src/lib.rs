574576059f401378e7968a1317dd1b178637d448
Tobias Klug <tbs.klg@gmail.com>
2025-08-24 16:40:28 +0200

    Solve nucleotide count

--- rust/nucleotide-count/src/lib.rs
+++ rust/nucleotide-count/src/lib.rs
@@ -1,31 +1,37 @@
 use std::collections::HashMap;
 
 pub fn count(nucleotide: char, dna: &str) -> Result<usize, char> {
-    if nucleotide == 'X' {
-        return Err('X');
+    if !matches!(nucleotide, 'A' | 'C' | 'G' | 'T') {
+        return Err(nucleotide);
     }
-    
+
+    let mut count = 0;
     for c in dna.chars() {
-        if c != 'A' && c != 'C' && c != 'G' && c != 'T' {
-            return Err(c);
+        match c {
+            'A' | 'C' | 'G' | 'T' => {
+                if c == nucleotide {
+                    count += 1;
+                }
+            }
+            invalid => return Err(invalid),
         }
     }
 
-    Ok(dna.chars().filter(|&c| c == nucleotide).count())
+    Ok(count)
 }
 
 pub fn nucleotide_counts(dna: &str) -> Result<HashMap<char, usize>, char> {
-    let mut counts = HashMap::new();
-    counts.insert('A', 0);
-    counts.insert('C', 0);
-    counts.insert('G', 0);
-    counts.insert('T', 0);
+    let mut counts = HashMap::from([('A', 0), ('C', 0), ('G', 0), ('T', 0)]);
 
     for c in dna.chars() {
-        if c != 'A' && c != 'C' && c != 'G' && c != 'T' {
-            return Err(c);
+        match c {
+            'A' | 'C' | 'G' | 'T' => {
+                *counts
+                    .get_mut(&c)
+                    .expect("Valid nucleotide should exist in map") += 1;
+            }
+            invalid => return Err(invalid),
         }
-        *counts.get_mut(&c).unwrap() += 1;
     }
 
     Ok(counts)
