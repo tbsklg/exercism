83b2079560b631a34353c8c77272fc1b81676785
Tobias Klug <tbs.klg@gmail.com>
2025-08-28 06:30:00 +0200

    Refactor: Degenerate case with no row containing 3 distinct groups of spaces

--- rust/diamond/src/lib.rs
+++ rust/diamond/src/lib.rs
@@ -1,11 +1,23 @@
+use std::iter::repeat_n;
+
 pub fn get_diamond(c: char) -> Vec<String> {
     if c == 'A' {
         return vec!["A".to_string()];
     }
 
-    vec![
-        " A ".to_string(),
-        "B B".to_string(),
-        " A ".to_string(),
-    ]
+    vec![first_line(c), middle_line(c), first_line(c)]
+}
+
+fn first_line(c: char) -> String {
+    let whitespaces = repeat_n(' ', alphabet_position(c)).collect::<String>();
+    format!("{}A{}", whitespaces, whitespaces)
+}
+
+fn middle_line(c: char) -> String {
+    let whitespaces = repeat_n(' ', alphabet_position(c)).collect::<String>();
+    format!("{}{}{}", c, whitespaces, c)
+}
+
+fn alphabet_position(c: char) -> usize {
+    (c as u8 - b'A') as usize
 }
