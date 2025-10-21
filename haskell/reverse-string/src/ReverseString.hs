0e8459eda1edf35ba3695bc5e49719a53a2080c4
Tobias Klug <tbs.klg@gmail.com>
2025-10-21 19:17:46 +0200

    Solve reverse string

--- haskell/reverse-string/src/ReverseString.hs
+++ haskell/reverse-string/src/ReverseString.hs
@@ -1,7 +1,4 @@
 module ReverseString (reverseString) where
 
 reverseString :: String -> String
-reverseString l = rev l []
-  where
-    rev [] a = a
-    rev (x : xs) a = rev xs (x : a)
+reverseString = reverse
--- haskell/reverse-string/test/Tests.hs
+++ haskell/reverse-string/test/Tests.hs
@@ -1,9 +1,10 @@
 {-# LANGUAGE RecordWildCards #-}
 
-import Data.Foldable (for_)
-import ReverseString (reverseString)
-import Test.Hspec (Spec, describe, it, shouldBe)
+import Data.Foldable     (for_)
+import Test.Hspec        (Spec, describe, it, shouldBe)
 import Test.Hspec.Runner (configFailFast, defaultConfig, hspecWith)
+
+import ReverseString (reverseString)
 
 main :: IO ()
 main = hspecWith defaultConfig {configFailFast = True} specs
@@ -11,47 +12,40 @@ main = hspecWith defaultConfig {configFailFast = True} specs
 specs :: Spec
 specs = describe "reverseString" $ for_ cases test
   where
-    test Case {..} = it explanation assertion
+
+    test Case{..} = it explanation assertion
       where
         explanation = unwords [show input, "-", description]
-        assertion = reverseString input `shouldBe` expected
+        assertion   = reverseString input `shouldBe` expected
 
-data Case = Case
-  { description :: String,
-    input :: String,
-    expected :: String
-  }
+data Case = Case { description :: String
+                 , input       :: String
+                 , expected    :: String
+                 }
 
 cases :: [Case]
-cases =
-  [ Case
-      { description = "empty string",
-        input = "",
-        expected = ""
-      },
-    Case
-      { description = "a word",
-        input = "robot",
-        expected = "tobor"
-      },
-    Case
-      { description = "a capitalized word",
-        input = "Ramen",
-        expected = "nemaR"
-      },
-    Case
-      { description = "a sentence with punctuation",
-        input = "I am hungry!",
-        expected = "!yrgnuh ma I"
-      },
-    Case
-      { description = "a palindrome",
-        input = "racecar",
-        expected = "racecar"
-      },
-    Case
-      { description = "an even-sized word",
-        input = "drawer",
-        expected = "reward"
-      }
-  ]
+cases = [ Case { description = "empty string"
+               , input       = ""
+               , expected    = ""
+               }
+        , Case { description = "a word"
+               , input       = "robot"
+               , expected    = "tobor"
+               }
+        , Case { description = "a capitalized word"
+               , input       = "Ramen"
+               , expected    = "nemaR"
+               }
+        , Case { description = "a sentence with punctuation"
+               , input       = "I am hungry!"
+               , expected    = "!yrgnuh ma I"
+               }
+        , Case { description = "a palindrome"
+               , input       = "racecar"
+               , expected    = "racecar"
+               }
+        , Case { description = "an even-sized word"
+               , input       = "drawer"
+               , expected    = "reward"
+               }
+        ]
